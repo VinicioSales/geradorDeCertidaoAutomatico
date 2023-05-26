f2dbcfcc27c1db66f291a71f23622c7a85c5cf9b
VinicioSales <vinicio.santtana@gmail.com>
2023-05-26 16:58:10 -0300

    Fazendo cabeçalho

--- src/funcs/geradores.py
+++ src/funcs/geradores.py
@@ -7,20 +7,36 @@ from reportlab.lib.colors import HexColor
 from reportlab.pdfbase.ttfonts import TTFont
 
 
-pdfmetrics.registerFont(TTFont('arialth', font_principal))
-
+pdfmetrics.registerFont(TTFont('font1', font_principal))
+pdfmetrics.registerFont(TTFont('font1_bold', font_principal_bold))
 
 def gerar_certidao_positiva():
     cnv = canvas.Canvas('CERTIDAO POSITIVA.pdf', pagesize=A4)
-    largura, altura = A4
-    cnv.roundRect(20, 20, largura-40, altura-40, 5, stroke=1, fill=0)
-    cnv.setFont('arialth', 11)
+    
+    #NOTE - logo
     img = Image.open(logo)
     width, height = img.size
-    escala = 0.5
+    escala = 0.2
     width = width * escala
     height = height * escala
-    cnv.drawImage(logo, x=100, y=100, width=width, height=height)
+    cnv.drawImage(logo, x=25, y=730, width=width, height=height)
+
+    #NOTE - Borda
+    largura, altura = A4
+    cnv.roundRect(20, 20, largura-40, altura-40, 10, stroke=1, fill=0)
+
+    #NOTE - Cabeçalho
+    x = 120
+    y = 805
+    cnv.setFont('font1_bold', 11)
+    cnv.drawString(x=x, y=y, text='ESTADO  BAHIA')
+    cnv.setFont('font1_bold', 12)
+    cnv.drawString(x=x, y=y-15, text='GRANDE ORIENTE DO BRASIL - BAHIA')
+    cnv.setFont('font1_bold', 11)
+    cnv.drawString(x=x, y=y-30, text='SECRETARIA DE FINANCAS GOB-BA')
+    cnv.setFont('font1_bold', 10)
+    cnv.drawString(x=x, y=y-45, text='RUA JOGO DO CARNEIRO, 157 - SAÚDE - 40.045-040')
+    cnv.drawString(x=x, y=y-60, text='CNPJ: 14670178000154')
 
 
     cnv.save()
--- src/static/variaveis.py
+++ src/static/variaveis.py
@@ -1,4 +1,5 @@
 from modules import caminhos
 
 font_principal = f'{caminhos.fonts}/ArialTh.ttf'
+font_principal_bold = f'{caminhos.fonts}/G_ari_bd.ttf'
 logo = f'{caminhos.img}/logo.jpg'
