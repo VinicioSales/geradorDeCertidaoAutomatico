b76de0e38adce7632464814b7c09818cc231d912
VinicioSales <vinicio.santtana@gmail.com>
2023-05-26 16:09:18 -0300

    Add borda

--- src/funcs/geradores.py
+++ src/funcs/geradores.py
@@ -12,6 +12,8 @@ pdfmetrics.registerFont(TTFont('arialth', font_principal))
 
 def gerar_certidao_positiva():
     cnv = canvas.Canvas('CERTIDAO POSITIVA.pdf', pagesize=A4)
+    largura, altura = A4
+    cnv.roundRect(20, 20, largura-40, altura-40, 5, stroke=1, fill=0)
     cnv.setFont('arialth', 11)
     img = Image.open(logo)
     width, height = img.size
