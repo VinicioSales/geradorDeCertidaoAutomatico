fa1b7c444dcac1343027003c2225ce2b8d1969d7
VinicioSales <vinicio.santtana@gmail.com>
2023-06-01 11:30:04 -0300

    Add tres pontos no ARLS

--- notes.md
+++ notes.md
@@ -2,7 +2,7 @@
 - Alterar dados do cabeçalho
 	- Onde tiver grande oriente do brasil alterar para nome novo (OK)
 	- ALterar endereço 
-- Colocar tres pontos entre as letras do A R L S (A∴R∴L∴S∴)
+- Colocar tres pontos entre as letras do A R L S (A∴R∴L∴S∴) (OK)
 - Alterar email do rodape - financeiro.delegacia@gmail.com
 - Alterar telefone do rodape - 71 9.992058-73
 - Alterar dadod do QR Code - Nome: Leandro dos Santos Araújo
--- src/backEnd/funcs/geradores.py
+++ src/backEnd/funcs/geradores.py
@@ -114,7 +114,7 @@ def gerar_certidao_positiva(cnpj_cpf: str, razao_social: str, endereco: str, mun
     y = 490
     cnv.setFont('font1', 12)
     cnv.drawString(x=x, y=y, charSpace=0.3, text='As informações disponíveis na Secretaria de Finanças do Grande Oriente do Brasil – Bahia /')
-    cnv.drawString(x=x, y=y-15, charSpace=0.45, text='GOB-BA, sobre a A R L S acima identificada são insuficientes para a emissão de Certidão')
+    cnv.drawString(x=x, y=y-15, charSpace=0.45, text='GOB-BA, sobre a A∴R∴L∴S∴ acima identificada são insuficientes para a emissão de Certidão')
     cnv.drawString(x=x, y=y-30, charSpace=0.3, text='Negativa ou Positiva Com Efeito Negativo de Débitos.')
     x = 290
     y = 375
@@ -213,7 +213,7 @@ def gerar_certidao_negativa(cnpj_cpf: str, razao_social: str, endereco: str, mun
     y = 490
     cnv.setFont('font1', 12)
     cnv.drawString(x=x, y=y, charSpace=0.3, text='Ressalvado o direito da Secretaria de Finanças do Grande Oriente do Brasil – Bahia / GOB-BA')
-    cnv.drawString(x=x, y=y-15, charSpace=0.45, text='cobrar e inscrever quaisquer dívidas de responsabilidade da A R L S acima identificada que')
+    cnv.drawString(x=x, y=y-15, charSpace=0.45, text='cobrar e inscrever quaisquer dívidas de responsabilidade da A∴R∴L∴S∴ acima identificada que')
     cnv.drawString(x=x, y=y-30, charSpace=0.3, text='vierem a ser apuradas, é Certificado que não constam pendências em seu nome, relativas as')
     cnv.drawString(x=x, y=y-45, charSpace=0.3, text='taxas administradas.')
     
@@ -323,7 +323,7 @@ def gerar_certidao_positiva_negativa(cnpj_cpf: str, razao_social: str, endereco:
     y = 490
     cnv.setFont('font1', 12)
     cnv.drawString(x=x, y=y, charSpace=0.3, text='Ressalvado o direito da Secretaria de Finanças do Grande Oriente do Brasil – Bahia / GOB-BA')
-    cnv.drawString(x=x, y=y-15, charSpace=0.45, text='cobrar e inscrever quaisquer dívidas de responsabilidade da A R L S acima identificada que')
+    cnv.drawString(x=x, y=y-15, charSpace=0.45, text='cobrar e inscrever quaisquer dívidas de responsabilidade da A∴R∴L∴S∴ acima identificada que')
     cnv.drawString(x=x, y=y-30, charSpace=0.3, text='vierem a ser apuradas, é Certificado que não constam pendências em seu nome, relativas as')
     cnv.drawString(x=x, y=y-45, charSpace=0.3, text='taxas administradas.')
     
