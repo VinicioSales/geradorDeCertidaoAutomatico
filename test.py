0ff1933cc39664a6c6b3e6504199b8025a49e04b
VinicioSales <vinicio.santtana@gmail.com>
2023-06-01 00:06:45 -0300

    Front pronto

--- test.py
+++
@@ -1,8 +0,0 @@
-import shutil
-import os
-
-diretorio_downloads = os.path.join(os.path.expanduser('~'), 'Documents')
-print(diretorio_downloads)
-print(type(diretorio_downloads))
-
-shutil.copy2(r'C:\Users\Parceiro\Meu Drive\Junior\Python\Omie\gerador_de_certidao_GRANDE_ORIENTE_DO_BRASIL\README.md', fr'{diretorio_downloads}\README.md')
