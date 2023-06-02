eaf9c2bd0655f5b803bebec8c2a4e8d7e098cc84
VinicioSales <vinicio.santtana@gmail.com>
2023-06-02 14:15:54 -0300

    Alterado cor butao

--- src/frontEnd/static/scripts/index.js
+++ src/frontEnd/static/scripts/index.js
@@ -5,7 +5,6 @@ const dropdownContent = document.querySelector('.dropdown-content')
 
 let nomeClientes = [];
 
-// Get the list of client names when the page loads
 fetch('http://localhost:5000/get_nome_clientes')
     .then(response => response.json())
     .then(data => nomeClientes = data);
@@ -18,13 +17,10 @@ razaoSocial.addEventListener('input', () => {
         const filteredClientes = nomeClientes.filter(cliente =>
             cliente.toLowerCase().includes(searchTerm)
         );
-
-        // Add each filtered client name to the dropdown
         for (let i = 0; i < filteredClientes.length; i++) {
             const div = document.createElement('div');
             div.innerHTML = filteredClientes[i];
             
-            // Add click event to each dropdown item
             div.addEventListener('click', function () {
                 razaoSocial.value = this.innerHTML;
                 dropdownContent.innerHTML = '';
